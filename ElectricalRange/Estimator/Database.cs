093c5203cee819c9f254368fc72818ee83b5848d
EngHassan <EngHassan@GN-00>
2025-06-14 21:36:13 +0300

    Refactor UI and remove unused resources
    This commit includes the removal of several `ResourceDictionary` entries in `App.xaml` related to the `GN.Base` library, along with the deletion of multiple image resources in `ElectricalRange.csproj`. The `Database.cs` file has been completely removed, indicating a refactor of database connection logic.
    
    Significant portions of code have been removed from model files such as `Category.cs`, `Class1.cs`, `DesignItem.cs`, and `Group.cs`, suggesting a cleanup of unused classes and properties. UI elements in `LoadingUpdatingDataControl.xaml`, `SelectGroupView.xaml`, and `SelectItemsView.xaml` have been simplified or redesigned.
    
    Minor adjustments were made to `DashboardView.xaml`, including style updates and the addition of `ImageSource` elements. Additionally, buttons and commands in `ItemsView.xaml` and `ItemsViewModel.cs` have been commented out or removed, reflecting changes in functionality for the digital library feature.

--- ElectricalRange/Estimator/Database.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProjectsNow.Estimator
-{
-    internal static class Database
-    {
-        //ProjectStore Database Connection String
-        public static readonly string ConnectionString = @"Server=tcp:erserver.database.windows.net,1433;Initial Catalog=ProjectsStore;Persist Security Info=False;User ID=erpcaps2022;Password=Wing00Gundam;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=0";
-    }
-}
