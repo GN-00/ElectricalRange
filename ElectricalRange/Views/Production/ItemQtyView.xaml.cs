ef3337d06a827c214733bbc2a2836e5b64fbb43c
EngHassan <EngHassan@GN-00>
2025-07-10 17:18:09 +0300

    Add ItemQtyView user control and view model
    This commit introduces a new user control `ItemQtyView` in XAML, designed to display item details such as code, description, quantity, stock, and post values. The control features specific styles for labels and numbers, along with save and cancel buttons. The code-behind file initializes the view model with necessary data. A new view model class `ItemQtyViewModel` is also created to manage item quantity logic and commands, ensuring proper data binding with the UI.

---
+++ ElectricalRange/Views/Production/ItemQtyView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ProjectsNow.Data.Production;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+
+namespace ProjectsNow.Views.Production
+{
+    public partial class ItemQtyView : UserControl, IPopup
+    {
+        public ItemQtyView(OrderItem item, ObservableCollection<OrderItem> orderItems, ObservableCollection<OrderItem> itemsToAdd)
+        {
+            InitializeComponent();
+            DataContext = new ItemQtyViewModel(item, orderItems, itemsToAdd);
+        }
+    }
+}
---
+++ ElectricalRange/Views/Production/ItemQtyViewModel.cs
@@ -0,0 +1,83 @@
+﻿using ProjectsNow.Commands;
+using ProjectsNow.Controllers;
+using ProjectsNow.Data;
+using ProjectsNow.Data.Application;
+using ProjectsNow.Data.Production;
+using System.Collections.ObjectModel;
+
+namespace ProjectsNow.Views.Production
+{
+    internal class ItemQtyViewModel : ViewModelBase
+    {
+        public ItemQtyViewModel(OrderItem item, ObservableCollection<OrderItem> orderItems, ObservableCollection<OrderItem> itemsToAdd)
+        {
+            Item = item;
+            NewData.Update(Item);
+            Items = orderItems;
+
+            SaveCommand = new RelayCommand(Save, CanSave);
+            CancelCommand = new RelayCommand(Cancel, CanCancel);
+        }
+
+        public double Posting
+        {
+            get => _Posting;
+            set
+            {
+                if (NewData.RemainingQty >= value)
+                {
+                    SetValue(ref _Posting, value)
+                   .UpdateProperties(this, nameof(GrossPrice));
+                }
+                else
+                {
+                    SetValue(ref _Posting, NewData.RemainingQty)
+                   .UpdateProperties(this, nameof(GrossPrice));
+                }
+            }
+        }
+
+        public OrderItem Item { get; }
+        public OrderItem NewData { get; } = new OrderItem();
+        public ObservableCollection<OrderItem> Items { get; }
+        public RelayCommand SaveCommand { get; }
+        public RelayCommand CancelCommand { get; }
+
+        private void Save()
+        {
+            NewData.InOrderQty += Posting;
+
+            CompanyPOTransaction newItem = new()
+            {
+                VAT = VAT,
+                Code = NewData.Code,
+                Description = NewData.Description,
+                Unit = NewData.Unit,
+                Qty = Posting,
+                Cost = NetPrice,
+            };
+
+            Item.Update(NewData);
+
+            if (Items != null)
+            {
+                Items.Add(newItem);
+            }
+
+            Navigation.ClosePopup();
+        }
+        private bool CanSave()
+        {
+            return true;
+        }
+
+        private void Cancel()
+        {
+            Navigation.ClosePopup();
+        }
+        private bool CanCancel()
+        {
+            return true;
+        }
+    }
+}
