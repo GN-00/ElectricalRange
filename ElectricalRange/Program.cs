f52c2bc47dcaeea03b8e4c0b2522279a4339de47
EngHassan <EngHassan@GN-00>
2025-12-04 11:37:09 +0300

    Update namespace and add app lifecycle instructions
    Added detailed comments in `Program.cs` under the `ProjectsNow` namespace to guide developers on using the Auto Updater. The comments include instructions for updating the `vpk` tool, publishing the application using `dotnet publish`, and packaging the app with `vpk pack`. These steps include specific parameters for app ID, version, output paths, executable names, and icons to streamline the update, publish, and release processes.
    
    Update namespace and add publishing instructions
    
    Updated the namespace in `Program.cs` to `ProjectsNow` to reflect a reorganization or alignment with the project's structure. Added detailed comments with instructions for using the `vpk` tool, including steps for updating the tool, publishing the app as a self-contained release, and packaging the app for release. The comments include examples for specifying app metadata such as ID, version, main executable, and icon.

--- ElectricalRange/Program.cs
+++ ElectricalRange/Program.cs
@@ -21,11 +21,9 @@ namespace ProjectsNow
 //If vpk commands aren't working, try the following:
 //dotnet tool update -g vpk
 
-
 //If you want to go with publishing the app, you can use the following commands: 
 //dotnet publish ElectricalRange.csproj -c Release --self-contained -r win-x64 -o .\publish
 //vpk pack -u YourAppId -v 1.0.0 -p.\publish -e yourMainApp.exe --icon ".\ElectricalRange\ER.ico"
-
 
 //If you want to go with Release the app, you can use the following commands: 
 //vpk pack -u ERI -v 1.2.5 -p.\ElectricalRange\bin\Release\net8.0-windows -e 'Electrical Range.exe' --icon ".\ElectricalRange\logo.ico"
